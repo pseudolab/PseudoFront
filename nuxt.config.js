67a080c519724600d3cea8b27e03e3b44917c8c3
seongpil0948 <seongpil0948@gmail.com>
2020-12-16 10:16:15 +0900

    to static web

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,6 +1,8 @@
 import colors from 'vuetify/es5/util/colors'
 
 export default {
+  // https://ko.nuxtjs.org/blog/going-full-static/
+  target: 'static',
   // Global page headers (https://go.nuxtjs.dev/config-head)
   head: {
     titleTemplate: '%s - PseudoFront',
--- pages/inspire.vue
+++ pages/inspire.vue
@@ -1,19 +1,6 @@
 <template>
   <!-- src="/v.png" -->
   <div>
-    <v-row justify="center">
-      <v-col cols="5">
-        <v-select
-          v-model="model"
-          :items="states"
-          :menu-props="{ maxHeight: '400' }"
-          label="Select"
-          multiple
-          hint="Pick your favorite states"
-          persistent-hint
-        ></v-select>
-      </v-col>
-    </v-row>
     <v-row>
       <v-col>
         <text-editor />
@@ -26,28 +13,10 @@
   import TextEditor from "@/components/api/quil/TextEditor.vue"
   export default {
     components: {
-      TextEditor
+      TextEditor,
     },
     data () {
-      return {
-        model: [],
-        states: [
-          'Alabama', 'Alaska', 'American Samoa', 'Arizona',
-          'Arkansas', 'California', 'Colorado', 'Connecticut',
-          'Delaware', 'District of Columbia', 'Federated States of Micronesia',
-          'Florida', 'Georgia', 'Guam', 'Hawaii', 'Idaho',
-          'Illinois', 'Indiana', 'Iowa', 'Kansas', 'Kentucky',
-          'Louisiana', 'Maine', 'Marshall Islands', 'Maryland',
-          'Massachusetts', 'Michigan', 'Minnesota', 'Mississippi',
-          'Missouri', 'Montana', 'Nebraska', 'Nevada',
-          'New Hampshire', 'New Jersey', 'New Mexico', 'New York',
-          'North Carolina', 'North Dakota', 'Northern Mariana Islands', 'Ohio',
-          'Oklahoma', 'Oregon', 'Palau', 'Pennsylvania', 'Puerto Rico',
-          'Rhode Island', 'South Carolina', 'South Dakota', 'Tennessee',
-          'Texas', 'Utah', 'Vermont', 'Virgin Island', 'Virginia',
-          'Washington', 'West Virginia', 'Wisconsin', 'Wyoming',
-        ],        
-      }
+      return {}
     },
   }
 </script>
