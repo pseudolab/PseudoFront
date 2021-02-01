27e15500cb242184439ee0d2eca898f0456f19bf
chicichich <pch6789@naver.com>
2021-02-01 16:33:03 +0900

    Add: HexagonGraph component

---
+++ components/profile/HexagonGraph.vue
@@ -0,0 +1,199 @@
+<template>
+  <canvas class="hexagon-graph" :width="width" :height="width"></canvas>
+</template>
+<script>
+export default {
+  props: {
+    width: {
+      type: Number,
+      default: 500,
+    },
+    fontSize: {
+      type: Number,
+      default: 20,
+    },
+    entries: {
+      type: Array,
+      default: () => [
+        'Post',
+        'Vote',
+        'Comment',
+        'Study Group',
+        'Answer',
+        'Question',
+      ],
+    },
+    activityScoreDataSet: {
+      type: Array,
+      default: () => [0, 0, 0, 0, 0, 0],
+    },
+    fontColor: {
+      type: String,
+      default: '#333',
+    },
+    color: {
+      type: String,
+      default: '#e0e0e0',
+    },
+    hasAnimation: {
+      type: Boolean,
+      defualt: false,
+    },
+  },
+  mounted() {
+    const ctx = this.$el.getContext('2d')
+    const {
+      width,
+      fontSize,
+      entries,
+      activityScoreDataSet,
+      hasAnimation,
+    } = this
+    const { calcPointPosition, calcInnerPointPosition } = this
+    ctx.font = `${fontSize}px Arial`
+    if (entries.length !== 6 || activityScoreDataSet.length !== 6) {
+      return console.warn('hexagonGraph component needs six entry.')
+    }
+    if (activityScoreDataSet.some((val) => val > 100)) {
+      return console.warn(
+        "hexagonGraph component's activityScoreDataSet values must be less than 100."
+      )
+    }
+    const maxDataVal = activityScoreDataSet.reduce(
+      (acc, cur) => (acc < cur ? cur : acc),
+      0
+    )
+    const space =
+      entries.reduce((acc, cur) => {
+        const curTextWidth = ctx.measureText(cur).width
+        if (acc < curTextWidth) return curTextWidth
+        return acc
+      }, 0) + 5
+    const sideLen = (width - 2 * space) / Math.sqrt(3)
+    const pointPositions = calcPointPosition(space, sideLen)
+    const halfPointPositions = calcInnerPointPosition(
+      sideLen,
+      Array(6).fill(50)
+    )
+
+    const curVal = hasAnimation ? 0 : maxDataVal
+    this.render(sideLen, pointPositions, halfPointPositions, maxDataVal, curVal)
+  },
+  methods: {
+    render(sideLen, pointPositions, halfPointPositions, maxDataVal, curVal) {
+      const ctx = this.$el.getContext('2d')
+      const { width, entries, activityScoreDataSet, color, fontColor } = this
+      const { calcInnerPointPosition } = this
+      ctx.clearRect(0, 0, width, width)
+      // draw outline
+      ctx.beginPath()
+      ctx.moveTo(pointPositions[0][0], pointPositions[0][1])
+      for (let i = 1; i < 6; i++) {
+        const [x, y] = pointPositions[i]
+        ctx.lineTo(x, y)
+      }
+      ctx.closePath()
+      ctx.stroke()
+
+      // draw text
+      ctx.fillStyle = fontColor
+      const textState = [
+        { textAlign: 'center', textBaseline: 'bottom' },
+        { textAlign: 'left', textBaseline: 'middle' },
+        { textAlign: 'left', textBaseline: 'middle' },
+        { textAlign: 'center', textBaseline: 'top' },
+        { textAlign: 'right', textBaseline: 'middle' },
+        { textAlign: 'right', textBaseline: 'middle' },
+      ]
+      pointPositions.forEach(([x, y], idx) => {
+        ctx.textAlign = textState[idx].textAlign
+        ctx.textBaseline = textState[idx].textBaseline
+        ctx.fillText(entries[idx], x, y)
+      })
+
+      const innerPointPositions = calcInnerPointPosition(
+        (sideLen / 100) * 95,
+        activityScoreDataSet.map((val) => (val > curVal ? curVal : val))
+      )
+      // fill inner graph
+      ctx.beginPath()
+      ctx.moveTo(innerPointPositions[0][0], innerPointPositions[0][1])
+      ctx.fillStyle = color
+      for (let i = 1; i < 6; i++) {
+        const [x, y] = innerPointPositions[i]
+        ctx.lineTo(x, y)
+      }
+      ctx.closePath()
+      ctx.fill()
+
+      // draw innerline
+      pointPositions.forEach(([x, y]) => {
+        ctx.beginPath()
+        ctx.moveTo(width / 2, width / 2)
+        ctx.lineTo(x, y)
+        ctx.stroke()
+      })
+
+      ctx.beginPath()
+      ctx.moveTo(halfPointPositions[0][0], halfPointPositions[0][1])
+      for (let i = 1; i < 6; i++) {
+        const [x, y] = halfPointPositions[i]
+        ctx.lineTo(x, y)
+      }
+      ctx.closePath()
+      ctx.stroke()
+      if (maxDataVal > curVal)
+        window.requestAnimationFrame(
+          this.render.bind(
+            this,
+            sideLen,
+            pointPositions,
+            halfPointPositions,
+            maxDataVal,
+            curVal + 1
+          )
+        )
+    },
+    calcInnerPointPosition(sideLen, activityScoreDataSet) {
+      const sqrt3 = Math.sqrt(3)
+      const { width } = this
+      const [originX, originY] = [width / 2, width / 2]
+      const datasetsRate = activityScoreDataSet.map(
+        (val) => sideLen * (val / 100)
+      )
+      return [
+        [originX, originY - datasetsRate[0]],
+        [
+          originX + (datasetsRate[1] / 2) * sqrt3,
+          originY - datasetsRate[1] / 2,
+        ],
+        [
+          originX + (datasetsRate[2] / 2) * sqrt3,
+          originY + datasetsRate[2] / 2,
+        ],
+        [originX, originY + datasetsRate[3]],
+        [
+          originX - (datasetsRate[4] / 2) * sqrt3,
+          originY + datasetsRate[4] / 2,
+        ],
+        [
+          originX - (datasetsRate[5] / 2) * sqrt3,
+          originY - datasetsRate[5] / 2,
+        ],
+      ]
+    },
+    calcPointPosition(space, sideLen) {
+      const { width } = this
+      return [
+        [width / 2, width / 2 - sideLen],
+        [width - space, width / 2 - sideLen / 2],
+        [width - space, width / 2 + sideLen / 2],
+        [width / 2, width / 2 + sideLen],
+        [space, width / 2 + sideLen / 2],
+        [space, width / 2 - sideLen / 2],
+      ]
+    },
+  },
+}
+</script>
+<style lang="scss" scoped></style>
---
+++ mock/profile/activityScoreData.js
@@ -0,0 +1,4 @@
+const activityScoreDataMaxScore = [700, 500, 600, 300, 1000, 400]
+const activityScoreDatascore = [700, 100, 400, 100, 500, 300]
+
+export { activityScoreDataMaxScore, activityScoreDatascore }
