85d8a1211061fa16e242209b9b2b9d079b3498d1
chicichich <pch6789@naver.com>
2021-02-01 16:33:03 +0900

    Add: HexagonGraph component

---
+++ pages/profile/index.vue
@@ -0,0 +1,37 @@
+<template>
+  <section class="profile">
+    <HexagonGraph
+      :activity-score-data-set="activityScoreDataSet"
+      :has-animation="true"
+    />
+  </section>
+</template>
+<script>
+import headMixin from '@/mixins/common/head.js'
+import HexagonGraph from '@/components/profile/HexagonGraph.vue'
+
+// mock data
+import {
+  activityScoreDataMaxScore,
+  activityScoreDatascore,
+} from '@/mock/profile/activityScoreData.js'
+
+export default {
+  components: {
+    HexagonGraph,
+  },
+  mixins: [headMixin],
+  computed: {
+    activityScoreDataSet() {
+      return activityScoreDataMaxScore.map(
+        (_, idx) =>
+          (activityScoreDatascore[idx] / activityScoreDataMaxScore[idx]) * 100
+      )
+    },
+  },
+  head() {
+    return this.getHead({ pageName: 'profile' })
+  },
+}
+</script>
+<style lang="scss" scoped></style>
