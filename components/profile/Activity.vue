11e14975cadb21920afb7abcd1e1a7c8ac6589ef
chicichich <pch6789@naver.com>
2021-02-03 23:37:51 +0900

    Add: Activity component

---
+++ components/profile/Activity.vue
@@ -0,0 +1,44 @@
+<template>
+  <section class="activity">
+    <v-chip class="mb-4" label> Activity </v-chip>
+    <article>
+      <ActivityCalender :year="2021" :calender-dataset="calenderDataset" />
+      <HexagonGraph
+        :activity-score-data-set="activityScoreDataSet"
+        :has-animation="true"
+        color="#0077a3b0"
+      />
+    </article>
+  </section>
+</template>
+<script>
+import HexagonGraph from '@/components/profile/HexagonGraph.vue'
+import ActivityCalender from '@/components/profile/ActivityCalender.vue'
+
+// mock data
+import {
+  activityScoreDataMaxScore,
+  activityScoreDatascore,
+} from '@/mock/profile/activityScoreData.js'
+import { calenderDataset } from '@/mock/profile/activityCalenderData.js'
+export default {
+  components: {
+    HexagonGraph,
+    ActivityCalender,
+  },
+  data() {
+    return {
+      calenderDataset,
+    }
+  },
+  computed: {
+    activityScoreDataSet() {
+      return activityScoreDataMaxScore.map(
+        (_, idx) =>
+          (activityScoreDatascore[idx] / activityScoreDataMaxScore[idx]) * 100
+      )
+    },
+  },
+}
+</script>
+<style lang="scss" scoped></style>
