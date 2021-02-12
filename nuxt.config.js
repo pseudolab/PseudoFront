df79da57e73047fb14e42ddf4a6240f27c676298
seongpil0948 <seongpil0948@gmail.com>
2021-02-13 01:46:52 +0900

    ssr 에서 vuetify 자동 tree shaking 되는 현상.. 해결

--- nuxt.config.js
+++ nuxt.config.js
@@ -50,7 +50,7 @@ export default {
   // Vuetify module configuration (https://go.nuxtjs.dev/config-vuetify)
   vuetify: {
     customVariables: ['~/assets/variables.scss'],
-    // treeShake: true,
+    treeShake: false,
     theme: { 
       dark: false,
       themes: {
