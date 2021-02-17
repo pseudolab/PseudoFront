b1af95f276c9ab2d5ca32a2342399b5238ec4a97
chicichich <pch6789@naver.com>
2021-02-17 16:54:48 +0900

    Add: profile Post page

--- pages/profile/index.vue
+++ pages/profile/index.vue
@@ -1,7 +1,7 @@
 <template>
   <section class="profile d-flex my-15 justify-space-between">
     <UserInfo />
-    <div>
+    <div class="flex-grow-1 ml-5">
       <v-chip-group class="mb-5" mandatory @change="changeSelectedMenu">
         <v-chip
           v-for="(name, idx) in profileMenues"
@@ -15,6 +15,13 @@
         </v-chip>
       </v-chip-group>
       <Summary v-if="selectedMenuIdx === 0" />
+      <Table
+        v-else-if="selectedMenuIdx === 1"
+        :table-data="tableData"
+        :loading="loading"
+        @changeSortType="changeSortType"
+        @changePage="changePage"
+      />
     </div>
   </section>
 </template>
@@ -22,23 +29,38 @@
 import headMixin from '@/mixins/common/head.js'
 import UserInfo from '@/components/profile/UserInfo.vue'
 import Summary from '@/components/profile/Summary.vue'
+import Table from '@/components/common/Table.vue'
+
+// mock data
+import archiveTableData from '@/mock/archive/archiveTableData.js'
 
 export default {
   components: {
     UserInfo,
     Summary,
+    Table,
   },
   mixins: [headMixin],
   data() {
     return {
       profileMenues: ['Summary', 'Posts', 'Q & A', 'Rank', 'Bookmarks'],
       selectedMenuIdx: 0,
+      tableData: archiveTableData,
+      loading: false,
     }
   },
 
   methods: {
     changeSelectedMenu(idx) {
       this.selectedMenuIdx = idx
+    },
+    changeSortType(type) {
+      // TODO: sortType 관련 항목 설정
+      this.sortType = type
+    },
+    changePage(idx) {
+      // TODO: pageIdx 관련 항목 설정
+      this.pageIdx = idx
     },
   },
   head() {
