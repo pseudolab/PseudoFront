15e48a564984beb88eeaed9e07cce48cbcac95bb
seongpil0948 <seongpil0948@gmail.com>
2021-03-12 23:03:04 +0900

    첫줄 문제 해결 역시 오픈소스를 참고하는ㄱ ㅔ최고

--- components/input/editor/SimpleEditor.vue
+++ components/input/editor/SimpleEditor.vue
@@ -1,11 +1,17 @@
 <script>
-import marked from 'marked'
+// import marked from 'marked'
 export default {
   data() {
     return {
       html: '',
     }
   },
+  computed: {
+    editNode() {
+      return this.$refs.txtArea
+    },
+  },
+  mounted() {},
   methods: {
     format(cmd, value) {
       document.execCommand(cmd, true, value)
@@ -13,32 +19,42 @@ export default {
   },
   render(h) {
     const self = this
-    return h('div', {
-      ref: 'txtArea', // for element
-      attrs: {
-        contenteditable: true,
-        id: 'txtArea', // for node
+    return h(
+      'div',
+      {
+        ref: 'txtArea', // for element
+        attrs: {
+          contenteditable: true,
+          id: 'txtArea', // for node
+          role: 'textbox',
+        },
+        on: {
+          mousedown: (evt) => {
+            if (evt.button === 2) {
+              self.$emit('click', evt)
+            }
+          },
+          input: (evt) => {
+            // self.html = evt.target.innerHTML
+            // const node = window.getSelection().anchorNode
+            // if (!self.editNode.innerHTML.startsWith('<')) {
+            //   self.editNode.innerHTML =
+            //     '<div>' + self.editNode.innerHTML + '</div>'
+            // } else if (node.data) {
+            //   // node.parentElement.outerHTML = marked(node.data)
+            //   // node.parentElement.focus()
+            // }
+          },
+          keyup: (evt) => {
+            // console.log(`shiftKey: ${evt.shiftKey}\n keyCode: ${evt.keyCode}, \n evt:`,evt,currentTarget:',evt.currentTarget)
+            if (evt.keyCode === '91') {
+              self.format('bold')
+            }
+          },
+        },
       },
-      on: {
-        mousedown: (evt) => {
-          if (evt.button === 2) {
-            this.$emit('click', evt)
-          }
-        },
-        input: (evt) => {
-          // Selected Node
-          const node = window.getSelection().anchorNode
-          node.outerHTML = node.data ? marked(node.data) : null
-          self.html = evt.target.innerHTML
-        },
-        keyup: (evt) => {
-          // console.log(`shiftKey: ${evt.shiftKey}\n keyCode: ${evt.keyCode}, \n evt:`,evt,currentTarget:',evt.currentTarget)
-          if (evt.keyCode === '91') {
-            self.format('bold')
-          }
-        },
-      },
-    })
+      [h('p', [h('br')])]
+    )
   },
 }
 </script>
