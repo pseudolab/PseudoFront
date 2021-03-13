e874901b0460ec39834498db23bccfc8563e3246
seongpil0948 <seongpil0948@gmail.com>
2021-03-14 01:26:03 +0900

    id as text Content

--- pages/community/query/index.vue
+++ pages/community/query/index.vue
@@ -7,7 +7,7 @@
           <v-subheader>Sections</v-subheader>
           <v-list-item v-for="(section, i) in sections" :key="i">
             <v-list-item-content @click="goSect(section)">
-              <v-list-item-title v-text="section.name"></v-list-item-title>
+              <v-list-item-title v-text="section.id"></v-list-item-title>
             </v-list-item-content>
           </v-list-item>
         </v-list>
@@ -35,10 +35,7 @@ export default {
   },
   methods: {
     addSection(id) {
-      this.sections.push({
-        name: `section-${id}`,
-        id: `section-${id}`,
-      })
+      this.sections.push({ id })
     },
     goSect(section) {
       const id = section.id
