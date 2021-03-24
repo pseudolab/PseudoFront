7123da3a803b73f5d5756898ac73af51375f82dd
seongpil0948 <seongpil0948@gmail.com>
2021-03-24 20:18:30 +0900

    일단 목업 통신 테스트용

--- components/input/editor/PseudoEditor.vue
+++ components/input/editor/PseudoEditor.vue
@@ -7,7 +7,32 @@ export default {
   data() {
     return {
       menuActive: false,
+      files: [],
     }
+  },
+  methods: {
+    readSingleFile(file, returnType = 'url') {
+      const fr = new FileReader()
+      return new Promise((resolve, reject) => {
+        fr.onerror = () => {
+          fr.abort()
+          reject(new DOMException('Problem parsing input file.'))
+        }
+        fr.onload = () => {
+          resolve(fr.result)
+        }
+        if (returnType === 'url') {
+          fr.readAsDataURL(file)
+        } else if (returnType === 'txt') {
+          fr.read.readAsText(file)
+        } else {
+          reject(
+            new DOMException(`
+            returnType: '${returnType}' not matched in readSingleFile`)
+          )
+        }
+      })
+    },
   },
   render(h) {
     const self = this
@@ -19,6 +44,38 @@ export default {
         },
       },
       [
+        h('v-file-input', {
+          props: {
+            value: self.files,
+            'prepend-icon': 'mdi-camera',
+            hideInput: true,
+            multiple: true,
+            chips: true,
+          },
+          on: {
+            change: async (vals) => {
+              self.files = vals
+              const { node } = this.$refs.editor.getAnchorElements()
+              let newLine = 'Why not Adjusted ???????'
+              try {
+                const imgUrl = await self.readSingleFile(self.files)
+                const imgElement = h('v-img', {
+                  props: {
+                    src: imgUrl,
+                    contain: true,
+                    transition: true,
+                  },
+                })
+                newLine = h('p', [imgElement])
+              } catch (err) {
+                newLine = h('p', [err])
+              } finally {
+                // #FIXME: 이거 왜 OBJECT 로 쓰이냐.. 동적 엘리먼트 추가가 안되네..
+                node.parentElement.after(newLine)
+              }
+            },
+          },
+        }),
         h(
           'v-menu',
           {
