e987484a7a59de96f5fd7c55c80ab613c4dc8226
chicichich <pch6789@naver.com>
2021-04-27 11:13:08 +0900

    Refactor: CategoryCard 레이아웃 분리

--- components/archive/CategoryCard.vue
+++ components/archive/CategoryCard.vue
@@ -1,57 +1,66 @@
-<template>
-  <v-card
-    :style="colorVariable"
-    class="category-card pa-4 pointer"
-    rounded="xl"
-    @click="handleBook"
+<template class="category-card">
+  <CategoryCardLayOut
+    :color="color"
+    :progress-value="progress"
+    :builder-profile-img="builderProfileImg"
+    :builder="builder"
+    @cardClicked="handleBook"
   >
-    <h6>
-      {{ startDate | moment('ll') }} -
-      <span v-if="endDate">
-        {{ endDate | moment('ll') }}
-      </span>
-      <span v-else> ing </span>
-    </h6>
-    <v-btn
-      class="pop-up-btn"
-      icon
-      :color="changeTone(0)"
-      @click.prevent.stop="popModal"
+    <!-- <button
+      class="pre-season season"
+      @click.prevent.stop="fetchPreSeason"
       @mousedown.stop=""
       @touchstart.stop=""
-    >
-      <v-icon>mdi-information-outline</v-icon>
-    </v-btn>
-    <div class="text-center my-9">
+    />
+    <button
+      class="next-season season"
+      @click.prevent.stop="fetchNextSeason"
+      @mousedown.stop=""
+      @touchstart.stop=""
+    /> -->
+    <template #icon="{ changeTone }">
+      <v-btn
+        icon
+        :color="changeTone(0)"
+        @click.prevent.stop="popModal"
+        @mousedown.stop=""
+        @touchstart.stop=""
+      >
+        <v-icon>mdi-information-outline</v-icon>
+      </v-btn>
+    </template>
+    <template #period>
+      <h6>
+        {{ startDate | moment('ll') }} -
+        <span v-if="endDate">
+          {{ endDate | moment('ll') }}
+        </span>
+        <span v-else> ing </span>
+      </h6>
+    </template>
+    <template #title>
       <h1>{{ title }}</h1>
+    </template>
+    <template #description>
       <p>
-        {{ briefDescription }}
+        {{ description }}
       </p>
-    </div>
-    <h3>Progress</h3>
-    <progress max="100" :value="progress" />
-    <footer>
-      <strong>Builder&nbsp;</strong>
-      <UserProfile :img-url="builderProfileImg" :width="20" />
-      <span class="builder"> {{ builder }} </span>
-      <span class="member ml-auto">
+    </template>
+    <template #footer-rd-corner>
+      <span class="member">
         <v-icon small>mdi-account</v-icon>
         {{ numberMember }}
       </span>
-    </footer>
-  </v-card>
+    </template>
+  </CategoryCardLayOut>
 </template>
 <script>
-// TODO: 카드 이동 애니메이션 추가하기
-// TODO: 모달창 제작
-// TODO: 카드 생성할때 사용할 카드 컴포넌트 추가
-import UserProfile from '@/components/common/UserProfile.vue'
-
+import CategoryCardLayOut from '@/components/archive/CategoryCardLayOut.vue'
 export default {
-  components: { UserProfile },
+  components: { CategoryCardLayOut },
   props: {
     title: { type: String, required: true },
-    briefDescription: { type: String, default: '' },
+    description: { type: String, required: true },
     builder: { type: String, required: true },
     builderProfileImg: { type: String, required: true },
     numberMember: { type: Number, required: true },
@@ -80,94 +89,46 @@ export default {
       required: true,
     },
   },
-  computed: {
-    colorVariable() {
-      return {
-        '--color': this.changeTone(0),
-        '--bg-color': this.isDeep()
-          ? this.changeTone(-30)
-          : this.changeTone(150),
-        '--p-bg-color': this.isDeep() ? '#efefef' : '#fff',
-      }
-    },
-  },
   methods: {
-    isDeep() {
-      const [red, green, blue] = this.color
-      if (red + green + blue > 400) {
-        return true
-      }
-      return false
+    popModal() {
+      console.log('modal pop-up')
     },
-    changeTone(level) {
-      return this.color.reduce((acc, cur) => {
-        let colorCode = cur + level
-        if (colorCode < 0) colorCode = 0
-        if (colorCode > 255) colorCode = 255
-        colorCode = colorCode.toString(16)
-        if (colorCode.length < 2) {
-          colorCode = '0' + colorCode
-        }
-        return acc + colorCode
-      }, '#')
+    fetchPreSeason() {
+      console.log('pre season')
+    },
+    fetchNextSeason() {
+      console.log('next season')
     },
     handleBook() {
       // TODO: 페이지 이동 추가(빌더 소개, 스터디, 최근 게시물)
       // TODO: 데이터 바인딩 시키기
       console.log('book clicked')
     },
-    popModal() {
-      console.log('modal pop-up')
-    },
   },
 }
 </script>
 <style lang="scss" scoped>
 .category-card {
-  position: relative;
-  overflow: hidden;
-  width: 100%;
-  height: 300px;
-
-  background-color: var(--bg-color);
-  & .pop-up-btn {
-    position: absolute;
-    top: 10px;
-    right: 10px;
-  }
-  & progress {
-    width: 100%;
-    position: relative;
-    -webkit-appearance: none;
-    height: 7px;
-    &::-webkit-progress-bar {
-      border-radius: 7px;
-      background-color: var(--p-bg-color);
-    }
-    &::-webkit-progress-value {
-      border-radius: 7px;
-      background-color: var(--color);
-    }
-    &::after {
-      content: attr(value) '%';
-      font-weight: bold;
-      position: absolute;
-      right: 0;
-    }
-  }
-  & footer {
-    width: 100%;
-    border-top: thin solid var(--p-bg-color);
-    padding: 16px 25px;
+  & .member {
     display: flex;
-    align-items: center;
-    font-size: 12px;
-    position: absolute;
-    bottom: 0;
-    left: 0;
+    justify-content: center;
   }
-  & .pointer {
-    cursor: pointer;
+  & .season {
+    height: 10px;
+    width: 100%;
+    position: absolute;
+    background-color: rgb(201, 201, 201);
+    left: 0;
+    z-index: 3;
+    &:hover {
+      background-color: rgb(228, 228, 228);
+    }
+  }
+  & .pre-season {
+    top: 0;
+  }
+  & .next-season {
+    bottom: 0;
   }
 }
 </style>
