8c01457a2cdacebc5129d307a7e9ea290a62f33b
chicichich <pch6789@naver.com>
2021-04-27 16:37:09 +0900

    Update: CategoryCard가 너무 길지 않도록 레이아웃 수정

--- pages/archive/index.vue
+++ pages/archive/index.vue
@@ -168,7 +168,8 @@ export default {
 .archive-category {
   &__cards {
     display: grid;
-    grid-template-columns: repeat(auto-fit, minmax(300px, 1fr));
+    justify-content: center;
+    grid-template-columns: repeat(auto-fit, minmax(300px, 500px));
     gap: 30px;
   }
 }
